45bd867bf7f56b46f5027728c0d847969e755a21
Wade Christie <wade.christie@gmail.com>
2023-09-11 16:56:53 -0700

    Add flags to pem finder to limit what objects to search

--- tools/pemfinder/main.cpp
+++ tools/pemfinder/main.cpp
@@ -11,6 +11,8 @@
 
 #include <cppossl/pem.hpp>
 #include <cppossl/x509.hpp>
+#include <cppossl/x509_crl.hpp>
+#include <cppossl/x509_req.hpp>
 
 namespace {
 
@@ -56,6 +58,11 @@ bool findpem(std::istream& in, std::ostream& out)
 
 struct cliargs
 {
+    bool find_all { true };
+    bool find_private_keys { false };
+    bool find_x509 { false };
+    bool find_x509_crl { false };
+    bool find_x509_req { false };
     bool print_text { false };
     bool show_help { false };
 };
@@ -70,7 +77,31 @@ int main(int argc, char const* argv[])
     auto cli
         = lyra::cli()
               .add_argument(lyra::help(args.show_help).description(help_description))
-              .add_argument(lyra::opt(args.print_text).name("--print-text").help("Print certificate information."));
+              .add_argument(lyra::opt([&args](bool flag) {
+                  args.find_all = false;
+                  args.find_private_keys = true;
+              })
+                                .name("--keys")
+                                .help("Look for private key objects."))
+              .add_argument(lyra::opt([&args](bool flag) {
+                  args.find_all = false;
+                  args.find_x509_crl = true;
+              })
+                                .name("--crl")
+                                .help("Look for X.509 CRL objects."))
+              .add_argument(lyra::opt([&args](bool flag) {
+                  args.find_all = false;
+                  args.find_x509_req = true;
+              })
+                                .name("--req")
+                                .help("Look for X.X509 request objects."))
+              .add_argument(lyra::opt([&args](bool flag) {
+                  args.find_all = false;
+                  args.find_x509 = true;
+              })
+                                .name("--x509")
+                                .help("Look for X.509 objects."))
+              .add_argument(lyra::opt(args.print_text).name("--print-text").help("Print X.509 object information."));
 
     auto result = cli.parse({ argc, argv });
     if (!result)
@@ -85,27 +116,65 @@ int main(int argc, char const* argv[])
         return 0;
     }
 
-    if (args.print_text)
+    if (args.find_all && !args.print_text)
+    {
+        while (findpem(std::cin, std::cout))
+            continue;
+    }
+    else
     {
         for (std::stringstream buffer; findpem(std::cin, buffer); buffer = std::stringstream {})
         {
             auto const pem = buffer.str();
-            if (starts_with(pem, ossl::pem::x509_begin_line))
+            if ((args.find_all || args.find_x509) && starts_with(pem, ossl::pem::x509_begin_line))
             {
-                auto const cert = ossl::pem::load<ossl::x509_t>(pem);
                 std::cout << pem;
-                std::cout << ossl::pem::pem_begin_line_prefix << "CERTIFICATE INFO" << ossl::pem::pem_line_suffix
-                          << std::endl;
-                std::cout << ossl::print_text(cert);
-                std::cout << ossl::pem::pem_end_line_prefix << "CERTIFICATE INFO" << ossl::pem::pem_line_suffix
-                          << std::endl;
+
+                if (args.print_text)
+                {
+                    auto const cert = ossl::pem::load<ossl::x509_t>(pem);
+                    std::cout << ossl::pem::pem_begin_line_prefix << "CERTIFICATE INFO" << ossl::pem::pem_line_suffix
+                              << std::endl;
+                    std::cout << ossl::print_text(cert);
+                    std::cout << ossl::pem::pem_end_line_prefix << "CERTIFICATE INFO" << ossl::pem::pem_line_suffix
+                              << std::endl;
+                }
+            }
+            else if ((args.find_all || args.find_x509_crl) && starts_with(pem, ossl::pem::x509_crl_begin_line))
+            {
+                std::cout << pem;
+
+                if (args.print_text)
+                {
+                    auto const crl = ossl::pem::load<ossl::x509_crl_t>(pem);
+                    std::cout << ossl::pem::pem_begin_line_prefix << "X509 CRL INFO" << ossl::pem::pem_line_suffix
+                              << std::endl;
+                    std::cout << ossl::print_text(crl);
+                    std::cout << ossl::pem::pem_end_line_prefix << "X509 CRL INFO" << ossl::pem::pem_line_suffix
+                              << std::endl;
+                }
+            }
+            else if ((args.find_all || args.find_x509_req) && starts_with(pem, ossl::pem::x509_req_begin_line))
+            {
+                std::cout << pem;
+
+                if (args.print_text)
+                {
+                    auto const req = ossl::pem::load<ossl::x509_req_t>(pem);
+                    std::cout << ossl::pem::pem_begin_line_prefix << "CERTIFICATE REQUEST INFO"
+                              << ossl::pem::pem_line_suffix << std::endl;
+                    std::cout << ossl::print_text(req);
+                    std::cout << ossl::pem::pem_end_line_prefix << "CERTIFICATE REQUEST INFO"
+                              << ossl::pem::pem_line_suffix << std::endl;
+                }
+            }
+            else if ((args.find_all || args.find_private_keys)
+                && (starts_with(pem, ossl::pem::pkey_begin_line)
+                    || starts_with(pem, ossl::pem::encrypted_pkey_begin_line)))
+            {
+                std::cout << pem;
             }
         }
-    }
-    else
-    {
-        while (findpem(std::cin, std::cout))
-            continue;
     }
 
     return 0;
