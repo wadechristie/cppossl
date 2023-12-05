fc31545116386d28be8e0579401f392e2e60a142
Wade Christie <wade.christie@gmail.com>
2023-12-04 16:10:02 -0800

    Update copyright header

--- src/evp_pkey.cpp
+++ src/evp_pkey.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+// Copyright (c) Wade Christie and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 //
 
